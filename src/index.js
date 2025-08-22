fc785b14d46f1abfb33076212ea4b92bfc21893b
barkingx <94052616+barkingx@users.noreply.github.com>
2025-08-22 10:56:06 +0800

    refactor: inline some constants

--- src/index.js
+++ src/index.js
@@ -1,25 +1,19 @@
-import { html as prettierHtmlParser } from "./prettier/language-html/parser-html.js";
+import {html as prettierHtmlParser} from "./prettier/language-html/parser-html.js";
 import prettierHtmlPrinter from "./prettier/language-html/printer-html.js";
 import prettierHtmlOptions from "./prettier/language-html/options.js";
 
-const AST_FORMAT_NAME = "html-jinjax-ast";
-const EXTENSION_NAME = "jinja";
-const EXTENSION_NAME_WITH_DOT = ".jinja";
 const PLUGIN_KEY = "jinjax";
-const PLUGIN_KEY_PASCAL_CASED = "JinjaX";
 
 export default {
   languages: [
     {
-      name: PLUGIN_KEY_PASCAL_CASED,
+      name: "JinjaX",
       parsers: [PLUGIN_KEY],
-      extensions: [EXTENSION_NAME_WITH_DOT, ".html"],
-      vscodeLanguageIds: [EXTENSION_NAME],
+      extensions: [".html", ".jinja"],
+      vscodeLanguageIds: ["html", "jinja"],
     },
   ],
-  parsers: {
-    [PLUGIN_KEY]: { ...prettierHtmlParser, astFormat: AST_FORMAT_NAME },
-  },
-  printers: { [AST_FORMAT_NAME]: prettierHtmlPrinter },
+  parsers: {[PLUGIN_KEY]: prettierHtmlParser,},
+  printers: { [prettierHtmlParser.astFormat]: prettierHtmlPrinter },
   options: prettierHtmlOptions,
 };
